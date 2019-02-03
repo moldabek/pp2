4a39f92b4f12a71fd7f0b35c29067f9ed663564b
Beibarys Moldabekov <biba4756@gmail.com>
2019-02-04 01:52:02 +0300

    lab1

---
+++ week 1/Task 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool Priime(int n){
+            if (n == 1)
+            {
+                return false;
+            }
+           for(int i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (n % i != 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] ar = new int[n];
+
+        }
+    }
+}
---
+++ week 1/Task 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());//с консоли вводим длину для нашего массива
+            int[] ar = new int[n];//создаем массив
+            string[] s = Console.ReadLine().Split();//строка для нашего массива
+            for(int i = 0; i < ar.Length; i++)//пробегаемся по массиву чтобы превратить все string-овые числа в int-овые
+            {
+                ar[i] = int.Parse(s[i]);
+            }
+            for(int i = 0; i < ar.Length; i++)//пробегаемся чтобы дублировать числа
+            {
+                Console.Write("{0} {0} ", ar[i]);//"{0} {0} " отвечает за дублирование
+            }
+           Console.ReadLine();
+        }
+    }
+}
